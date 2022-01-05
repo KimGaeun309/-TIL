8b719d23bf568d174aeb348544378ff76766fbd1
KimGaeun309 <90085690+KimGaeun309@users.noreply.github.com>
2022-01-05 13:29:46 +0900

    Create BinaryPractice.java

---
+++ Java/2021-S-Standard_of_Java/ch04/BinaryPractice.java
@@ -0,0 +1,13 @@
+package ch04;
+
+public class BinaryPractice {
+
+	public static void main(String[] args) {
+		for(int i = 0; i <= 1; i++)
+			for(int j = 0; j <= 1; j++)
+				for(int k = 0; k <= 1; k++)
+					for(int l = 0; l <= 1; l++)
+						System.out.println(""+i+j+k+l);
+	}
+
+}
